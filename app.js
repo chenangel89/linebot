52e35646bc3d30bd393dc83c71c0c9304603711a
chenangel89 <76103256+chenangel89@users.noreply.github.com>
2023-04-07 16:16:22 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+const { Configuration, OpenAIApi } = require('@openai/api');
 require('dotenv').config();
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
+  baseApiUrl: "https://api.openai.com",
 });
 const openai = new OpenAIApi(configuration);
 
